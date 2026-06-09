55e688fffab68594dd5892c70dff811add50a473
indeed-yt <iamindeed27@gmail.com>
2026-06-09 17:49:46 +0530

    locatings parents

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -33,3 +33,53 @@ test("User facing locators", async ({ page }) => {
   await page.getByLabel("Email").first().click();
   await page.getByTestId("sign-in-button").click();
 });
+
+test("Locating child elements", async ({ page }) => {
+  await page.locator('nb-card nb-radio :text-is("Option 1")').click();
+  await page
+    .locator("nb-card")
+    .locator("nb-radio")
+    .locator(':text-is("Option 2")')
+    .click();
+  await page
+    .locator("nb-card")
+    .getByRole("button", { name: "SIGN IN" })
+    .first()
+    .click();
+  //try to avoid indexes since order of the elements sometimes can be changes
+  await page.locator("nb-card").nth(3).getByRole("button").click();
+});
+
+test("locating parent element", async ({ page }) => {
+  await page
+    .locator("nb-card", { hasText: "Using the Grid" })
+    .getByRole("textbox", { name: "Email" })
+    .click();
+  await page
+    .locator("nb-card", { has: page.locator("#inputEmail") })
+    .getByRole("textbox", { name: "Email" })
+    .click();
+  await page
+    .locator("nb-card")
+    .filter({ hasText: "Basic form" })
+    .getByRole("textbox", { name: "Email" })
+    .click();
+  await page
+    .locator("nb-card")
+    .filter({ has: page.locator(".status-danger") })
+    .getByRole("textbox", { name: "Password" })
+    .click();
+  await page
+    .locator("nb-card")
+    .filter({ has: page.locator("nb-checkbox") })
+    .filter({ hasText: "Sign in" })
+    .getByRole("textbox", { name: "Email" })
+    .click();
+  
+  //not really a recommended method
+  await page
+    .locator(':text-is("Using the Grid")')
+    .locator("..")
+    .getByRole("textbox", { name: "Email" })
+    .click();
+});
