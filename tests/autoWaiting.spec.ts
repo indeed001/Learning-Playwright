aac624243095498155bf52775cb443ef11158208
indeed-yt <iamindeed27@gmail.com>
2026-06-11 16:01:55 +0530

    auto waiting

---
+++ tests/autoWaiting.spec.ts
@@ -0,0 +1,37 @@
+import { expect, test } from "@playwright/test";
+
+test.beforeEach(async ({ page }) => {
+  await page.goto("http://uitestingplayground.com/ajax");
+  await page
+    .getByRole("button", { name: "Button Triggering AJAX Request" })
+    .click();
+});
+
+test("auto waiting", async ({ page }) => {
+  const successButton = page.locator(".bg-success");
+  //   await successButton.click();
+  //   const text = await successButton.textContent();
+  //   await successButton.waitFor({ state: "attached" });
+  //   const text = await successButton.allTextContents();
+  //   expect(text).toContain("Data loaded with AJAX get request.");
+
+  await expect(successButton).toHaveText("Data loaded with AJAX get request.", {
+    timeout: 20000,
+  });
+});
+
+test("Alternative waits", async ({ page }) => {
+  const successButton = page.locator(".bg-success");
+
+  //__wait for element
+  //   await page.waitForSelector(".bg-success");
+
+  //__wait for particular response
+//   await page.waitForResponse("http://uitestingplayground.com/ajaxdata");
+
+  //__wait for network to be idle (NOT RECOMMENDED)
+  await page.waitForLoadState('networkidle')
+  
+  const text = await successButton.allTextContents();
+  expect(text).toContain("Data loaded with AJAX get request.");
+});
