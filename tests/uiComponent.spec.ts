2346dd8c25b42992ff54d1a2ad27c4312ff3b5bb
indeed-yt <iamindeed27@gmail.com>
2026-06-13 01:07:37 +0530

    list and dropdowns

--- tests/uiComponent.spec.ts
+++ tests/uiComponent.spec.ts
@@ -62,3 +62,33 @@ test("checkbox", async ({ page }) => {
     expect(boxes.isChecked()).toBeTruthy();
   }
 });
+
+test("list and dropdowns", async ({ page }) => {
+  const dropDownMenu = page.locator("ngx-header nb-select .select-button");
+  await dropDownMenu.click();
+
+  page.getByRole("list"); //when the list has a UL tag
+  page.getByRole("listitem"); //when the list has LI tag
+
+  // const optionList = page.getByRole("list").locator("nb-option");
+  const optionList = page.locator("nb-option-list nb-option");
+  await expect(optionList).toHaveText(["Light", "Dark", "Cosmic", "Corporate"]);
+  await optionList.filter({ hasText: "Cosmic" }).click();
+
+  const header = page.locator("nb-layout-header");
+  await expect(header).toHaveCSS("background-color", "rgb(50, 50, 89)");
+
+  const colors = {
+    Light: "rgb(255, 255, 255)",
+    Dark: "rgb(34, 43, 69)",
+    Cosmic: "rgb(50, 50, 89)",
+    Corporate: "rgb(255, 255, 255)",
+  };
+
+  await dropDownMenu.click();
+  for (const [theme, color] of Object.entries(colors)) {
+    await optionList.filter({ hasText: theme }).click();
+    await expect(header).toHaveCSS("background-color", color);
+    if (theme != "Corporate") await dropDownMenu.click();
+  }
+});
